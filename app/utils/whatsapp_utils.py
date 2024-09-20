28e56e522fb4b833c6291cc18e50a7692e2cb004
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2024-09-20 08:09:04 +0530

    Update whatsapp_utils.py

--- app/utils/whatsapp_utils.py
+++ app/utils/whatsapp_utils.py
@@ -252,7 +252,7 @@ def generate_response(response):
     
         
     
-    return generate_content("gemini-pro",response)
+    return generate_content("gemini-1.0-pro",response)
 
 def send_message(data):
     headers = {
